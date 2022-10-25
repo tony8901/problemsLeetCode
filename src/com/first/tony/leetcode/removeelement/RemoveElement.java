6fd863b5260175d011780d79c50cf9adc2def434
tony8901 <tony.garces89@gmail.com>
2022-10-25 12:46:18 -0400

    problema RemoveElement

---
+++ src/com/first/tony/leetcode/removeelement/RemoveElement.java
@@ -0,0 +1,47 @@
+package com.first.tony.leetcode.removeelement;
+
+public class RemoveElement {
+    public RemoveElement() {
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        if(nums.length == 0 || nums.length == 1 && nums[0] == val) return k;
+        else if(nums.length == 1 && nums[0] != val) return ++k;
+
+        for (int i = 0; i < nums.length-1; i++) {
+            if(nums[i] != val){
+                if(i == nums.length-2 && nums[nums.length-1] != val) k++;
+                k++;
+            } else {
+                for (int j = i+1; j < nums.length; j++) {
+                    if(nums[j] != val){
+                        nums[i] = nums[j];
+                        nums[j] = val;
+                        k++;
+                        break;
+                    } else if (j == nums.length - 1) {
+                        return ++k;
+                    }
+                }
+            }
+        }
+        return k;
+    }
+
+    /*Respuesta en l foro de discusion (hay que revisarla pero creo que es la mas optima)
+    public int removeElement(int[] nums, int val) {
+
+    int nextPosition = 0;
+
+    for(int i=0; i<nums.length; i++) {
+        if(nums[i] != val) {
+            nums[nextPosition] = nums[i];
+            nextPosition++;
+        }
+    }
+    return nextPosition;
+}
+    * */
+
+}
--- src/com/first/tony/main.java
+++ src/com/first/tony/main.java
@@ -1,21 +1,14 @@
 package com.first.tony;
-
-import com.first.tony.leetcode.TwoSum.TwoSum;
-import com.first.tony.leetcode.ispalindrome.IsPalindrome;
-import com.first.tony.leetcode.longestcommonprefix.LongComPrefix;
-import com.first.tony.leetcode.removeduplicates.RemoveDuplicates;
-import com.first.tony.leetcode.romantointeger.RomanToInteger;
-import com.first.tony.leetcode.validparentheses.IsValid;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import com.first.tony.leetcode.removeelement.RemoveElement;
 public class main {
     public static void main (String[] args){
 
-        RemoveDuplicates x = new RemoveDuplicates();
-        int[] nums = {-1,0,0,0,0,3,3};
-        System.out.println(x.removeDuplicates(nums));
+        RemoveElement x = new RemoveElement();
+        int[] nums = {3,3};
+        int val = 5;
+        System.out.println(x.removeElement(nums,val));
+        System.out.print("[ ");
         for (int y : nums) System.out.print(y + " ");
+        System.out.println("]");
     }
 }
